f7a081c0f1bd50077ce17e471f7a6f1955e89fd1
okendi <okendi78993012@gmail.com>
2023-09-06 10:55:15 +0000

    mon premier app

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,48 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><span>Communi</span>Arts</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html" class="active">Accueil</a></li>
+
+          <li class="dropdown"><a href="#"><span>Catalogue des talents</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="Artistes.html">Artistes</a></li>
+              <li><a href="Sculpture.html">Sculpture</a></li>
+              <li><a href="Danse.html">Danse</a></li>
+              <li><a href="Peinture.html">Peinture</a></li>
+              <li><a href="Photographie.html">Photographie</a></li>
+              <li><a href="Artisanat.html">Artisanat</a></li>
+              <li><a href="Comedie et Cinema.html">Comedie et Cinema</a></li>
+              <!--li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              <li-->
+            </ul>
+          </li>
+
+          <li><a href="services.html">Services</a></li>
+          <li><a href="portfolio.html">Portefeuille</a></li>
+          <li><a href="calendrier.html">Calendrier</a></li>
+          <li><a href="blog.html">Blogue</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="membre.html">Devenir-Membre</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+     
+
+    </div>
+  </header><!-- End Header -->
